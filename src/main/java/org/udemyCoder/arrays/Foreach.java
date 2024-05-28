c0580a294cd5ec87538d542b9d6e52e5e3e626af
Alym62 <gasafm62@gmail.com>
2024-05-28 20:38:01 -0300

    feat: adicionado foreach.

---
+++ src/main/java/org/udemyCoder/arrays/Foreach.java
@@ -0,0 +1,11 @@
+package org.udemyCoder.arrays;
+
+public class Foreach {
+    public static void main(String[] args) {
+        double[] notas = {1.6, 4.6, 9.5};
+
+        for (double nota : notas) {
+            System.out.println(nota);
+        }
+    }
+}
