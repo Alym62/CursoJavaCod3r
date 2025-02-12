ed138cc522e4e82aecf7b396074624f419c84065
Alyasaf Meireles <gasafm62@gmail.com>
2025-02-12 17:55:12 -0300

    feat: desafio proposto na aula.

---
+++ src/main/java/org/udemyCoder/streams/DesafioMap.java
@@ -0,0 +1,15 @@
+package org.udemyCoder.streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DesafioMap {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        /*
+        * 1 - Número para string binária... 6 => "110"
+        * 2 - Inverter a string... "110" => "011"
+        * 3 - Converter novamente para inteiro => 3
+        * */
+    }
+}
